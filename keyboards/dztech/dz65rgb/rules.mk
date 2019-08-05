c5d626d05d4c327a5542a41c24d2e394b74f28a8
Fiona Cutler <fiona.phocidae@outlook.com>
2019-08-04 18:40:20 -0600

    Updated

--- keyboards/dztech/dz65rgb/rules.mk
+++ keyboards/dztech/dz65rgb/rules.mk
@@ -10,5 +10,4 @@ COMMAND_ENABLE = no                # Commands for debug and configuration
 NKRO_ENABLE = yes                 # USB Nkey Rollover
 AUDIO_ENABLE = no
 RGB_MATRIX_ENABLE = yes     # Use RGB matrix
-
 LAYOUTS = 65_ansi
