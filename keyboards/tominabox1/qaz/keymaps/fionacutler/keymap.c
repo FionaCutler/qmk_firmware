61a3916ab3c2af7e1d65f261417cdfb52fee7263
Fiona <fiona.r.cutler@outlook.com>
2026-05-12 21:38:12 -0600

    tinkering  with  qaz

---
+++ keyboards/tominabox1/qaz/keymaps/fionacutler/keymap.c
@@ -0,0 +1,46 @@
+#include QMK_KEYBOARD_H
+
+enum layers{
+  _BASE,
+  _NUM_SYM,
+  _NAV
+};
+enum combo_events {
+  COMBO_BSPC,
+  COMBO_NUMBAK,
+  COMBO_TAB,
+  COMBO_ESC,
+  COMBO_DEL,
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [_BASE] = LAYOUT_split_space(
+    KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,    KC_I,   KC_O,     KC_P,
+    KC_A,  KC_S,  KC_D,  KC_F,   KC_G,   KC_H,  KC_J,   KC_K,  KC_L, KC_ENT,
+    KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M, KC_COMM, KC_DOT,
+    KC_LCTL, KC_LALT, KC_LGUI, KC_SPACE, LT(_NUM_SYM,KC_BSPC), KC_RALT, KC_RGUI, KC_RCTL
+  ),
+  [_NUM_SYM] = LAYOUT_split_space(
+    KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0,
+    KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_EQUAL, KC_MINS,
+    KC_LCBR, KC_LBRC, KC_LPRN, KC_UNDS, KC_RPRN, KC_RBRC, KC_RCBR, KC_SCLN, KC_QUOTE,
+    UG_TOGG, UG_NEXT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+  )
+};
+
+#ifdef COMBO_ENABLE
+const uint16_t PROGMEM combo_bspc[] = {KC_O, KC_P, COMBO_END};
+const uint16_t PROGMEM combo_numbak[] = {KC_0, KC_9, COMBO_END};
+const uint16_t PROGMEM combo_tab[] = {KC_Q, KC_W, COMBO_END};
+const uint16_t PROGMEM combo_esc[] = {KC_E, KC_W, COMBO_END};
+const uint16_t PROGMEM combo_del[] = {KC_MINS, KC_EQL, COMBO_END};
+
+combo_t key_combos[] = {
+  [COMBO_BSPC] = COMBO(combo_bspc,KC_BSPC),
+  [COMBO_NUMBAK] = COMBO(combo_numbak,KC_BSPC),
+  [COMBO_TAB] = COMBO(combo_tab,KC_TAB),
+  [COMBO_ESC] = COMBO(combo_esc,KC_ESC),
+  [COMBO_DEL] = COMBO(combo_del,KC_DEL),
+
+};
+#endif
